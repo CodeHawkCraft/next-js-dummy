35f751182654d5554a52dc96ef89d9a25a5ea4cb
Davinder Kumar <davinder.kumar.code@gmail.com>
2025-04-12 01:18:57 +0530

    added changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="flex items-center justify-center">
-      <h1 className="font-bold text-3xl text-red-500 ">Dummy website brother</h1>
+      <h1 className="font-bold text-3xl text-red-500 ">Dummy next js web app</h1>
     </div>
   );
 }
